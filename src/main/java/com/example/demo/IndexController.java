6027d45e210e0842482a81b323bdd9fcf2cf5530
NgaTT <ngatt@newwave.vn>
2019-12-25 11:14:37 +0700

    Hello

--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @GetMapping("/")
     public String index() {
-        return "Hello there! I'm running.";
+        return "Hesdsdsdsdllo there! I'm running.\n ddddddddddddddddddddddddddddddd";
     }
 }
